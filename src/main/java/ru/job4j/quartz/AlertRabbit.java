d0fcb92fb1a5850849d3d180d499cd120127bcfd
lenarsharipov <lenarsharipov@gmail.com>
2022-11-03 18:47:27 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,10 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -11,6 +15,7 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
     private static Properties getProperties() {
         var config = new Properties();
         try (var in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
@@ -20,28 +25,61 @@ public class AlertRabbit {
         }
         return config;
     }
-    public static void main(String[] args) {
-        try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName(getProperties().getProperty("driver-class-name"));
+        try (var cn = DriverManager.getConnection(
+                getProperties().getProperty("url"),
+                getProperties().getProperty("username"),
+                getProperties().getProperty("password"))) {
+            var scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
+            var data = new JobDataMap();
+            data.put("connection", cn);
+            var job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+
+            var times = simpleSchedule()
                     .withIntervalInSeconds(Integer.parseInt(getProperties().getProperty("rabbit.interval")))
                     .repeatForever();
-            Trigger trigger = newTrigger()
+
+            var trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
+
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            Thread.sleep(10000);
+            scheduler.shutdown();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+
+        private final LocalDateTime created;
+
+        public Rabbit() {
+            created = LocalDateTime.now();
+        }
+
+        public LocalDateTime getCreated() {
+            return created;
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+            var cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (var statement =
+                         cn.prepareStatement("insert into rabbit(created_date) values (?)")) {
+                statement.setTimestamp(1, Timestamp.valueOf(getCreated()));
+                statement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
