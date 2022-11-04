0da9a61736a913adfe53d562922425919dcdcda5
lenarsharipov <lenarsharipov@gmail.com>
2022-11-04 19:55:02 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -25,13 +25,28 @@ public class HabrCareerParse {
                 var linkElement = titleElement.child(0);
                 var vacancyName = titleElement.text();
                 var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-
                 var dateElement = row.select(".vacancy-card__date").first();
                 var vacancyDateElement = dateElement.child(0);
                 var dateTime = String.format("%s", vacancyDateElement.attr("datetime"));
-                System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
+
+                try {
+                    var descr = retrieveDescription(link);
+                    System.out.printf("%s %s %s%n%s%n", vacancyName, link, dateTime, descr);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             });
             page++;
         }
+    }
+
+    private static String retrieveDescription(String link) throws IOException {
+            var connection = Jsoup.connect(link);
+            var document = connection.get();
+            var rows = document.select(".vacancy-show");
+            var sb = new StringBuilder();
+            sb.append(rows.select(".section-title__title").first().text().concat(System.lineSeparator()));
+            sb.append(rows.select(".style-ugc").text());
+            return sb.toString();
     }
 }
