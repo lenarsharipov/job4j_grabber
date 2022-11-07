3c5b7918e055ba0af0a13084fa4c50f18241227e
lenarsharipov <lenarsharipov@gmail.com>
2022-11-07 23:17:32 +0300

    6. Grabber. [#289477]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,10 +1,13 @@
 package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
@@ -13,6 +16,61 @@ public class HabrCareerParse {
     private static final int PAGES = 5;
 
     private static final String DELIMITER = "?page=";
+
+    private static final List<Post> POSTS = new ArrayList<>();
+
+    private static HabrCareerDateTimeParser dateTimeParser;
+
+    public HabrCareerParse(HabrCareerDateTimeParser habrCareerDateTimeParser) {
+        dateTimeParser = habrCareerDateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        try {
+            parsePosts();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return POSTS;
+    }
+
+    private static void parsePosts() throws IOException {
+        for (var page = 1; page <= PAGES; page++) {
+            var connection = Jsoup.connect(String.format("%s%s%s", PAGE_LINK, DELIMITER, page));
+            var document = connection.get();
+            var rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                var titleElement = row.select(".vacancy-card__title").first();
+                var linkElement = titleElement.child(0);
+                var vacancyName = titleElement.text();
+                var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                var dateElement = row.select(".vacancy-card__date").first();
+                var vacancyDateElement = dateElement.child(0);
+                var dateTime = String.format("%s", vacancyDateElement.attr("datetime"));
+                String description;
+                try {
+                    description = retrieveDescription(link);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                POSTS.add(new Post(
+                        vacancyName,
+                        link,
+                        description,
+                        dateTimeParser.parse(dateTime)
+                ));
+            });
+        }
+    }
+
+    private static String retrieveDescription(String link) throws IOException {
+        var connection = Jsoup.connect(link);
+        var document = connection.get();
+        var rows = document.select(".vacancy-show");
+        return rows.select(".style-ugc").text();
+    }
+
 
     public static void main(String[] args) throws IOException {
         var rowSeparator = "*".repeat(60);
@@ -40,10 +98,4 @@ public class HabrCareerParse {
         }
     }
 
-    private static String retrieveDescription(String link) throws IOException {
-            var connection = Jsoup.connect(link);
-            var document = connection.get();
-            var rows = document.select(".vacancy-show");
-            return rows.select(".style-ugc").text();
-    }
 }
