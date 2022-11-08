c45424a27ccf10cef5f101e5eca4464fb21f5b27
lenarsharipov <lenarsharipov@gmail.com>
2022-11-08 18:15:02 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,6 +1,8 @@
 package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
@@ -9,59 +11,54 @@ import java.util.List;
 
 public class HabrCareerParse implements Parse {
 
-    private static final String SOURCE_LINK = "https://career.habr.com";
-
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
-
     private static final int PAGES = 5;
 
     private static final String DELIMITER = "?page=";
 
-    private static final List<Post> POSTS = new ArrayList<>();
-
-    private static HabrCareerDateTimeParser dateTimeParser;
+    private final DateTimeParser dateTimeParser;
 
     public HabrCareerParse(HabrCareerDateTimeParser habrCareerDateTimeParser) {
         dateTimeParser = habrCareerDateTimeParser;
     }
 
     @Override
-    public List<Post> list(String link) {
-        try {
-            parsePosts();
-        } catch (Exception e) {
-            e.printStackTrace();
+    public List<Post> list(String sourceLink) {
+        List<Post> posts = new ArrayList<>();
+        var pageLink = String.format("%s/vacancies/java_developer", sourceLink);
+        for (var page = 1; page <= PAGES; page++) {
+            try {
+                var connection = Jsoup.connect(String.format("%s%s%s", pageLink, DELIMITER, page));
+                var document = connection.get();
+                var rows = document.select(".vacancy-card__inner");
+                for (var element : rows) {
+                    posts.add(
+                            parsePost(sourceLink, element)
+                    );
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        return POSTS;
+        return posts;
     }
 
-    private static void parsePosts() throws IOException {
-        for (var page = 1; page <= PAGES; page++) {
-            var connection = Jsoup.connect(String.format("%s%s%s", PAGE_LINK, DELIMITER, page));
-            var document = connection.get();
-            var rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                var titleElement = row.select(".vacancy-card__title").first();
-                var linkElement = titleElement.child(0);
-                var vacancyName = titleElement.text();
-                var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                var dateElement = row.select(".vacancy-card__date").first();
-                var vacancyDateElement = dateElement.child(0);
-                var dateTime = String.format("%s", vacancyDateElement.attr("datetime"));
-                String description;
-                try {
-                    description = retrieveDescription(link);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                POSTS.add(new Post(
-                        vacancyName,
-                        link,
-                        description,
-                        dateTimeParser.parse(dateTime)
-                ));
-            });
-        }
+    private Post parsePost(String sourceLink, Element element) throws IOException {
+
+        var titleElement = element.select(".vacancy-card__title").first();
+        var linkElement = titleElement.child(0);
+        var vacancyName = titleElement.text();
+        var subLink = String.format("%s%s", sourceLink, linkElement.attr("href"));
+        var dateElement = element.select(".vacancy-card__date").first();
+        var vacancyDateElement = dateElement.child(0);
+        var dateTime = String.format("%s", vacancyDateElement.attr("datetime"));
+        var description = retrieveDescription(subLink);
+        return new Post(
+                vacancyName,
+                subLink,
+                description,
+                dateTimeParser.parse(dateTime)
+        );
+
     }
 
     private static String retrieveDescription(String link) throws IOException {
@@ -71,30 +68,11 @@ public class HabrCareerParse implements Parse {
         return rows.select(".style-ugc").text();
     }
 
-
-    public static void main(String[] args) throws IOException {
-        var rowSeparator = "*".repeat(60);
-        for (var page = 1; page <= PAGES; page++) {
-            var connection = Jsoup.connect(String.format("%s%s%s", PAGE_LINK, DELIMITER, page));
-            var document = connection.get();
-            var rows = document.select(".vacancy-card__inner");
-            System.out.printf("%n%s Page: %s %s%n%n", rowSeparator, page, rowSeparator);
-            rows.forEach(row -> {
-                var titleElement = row.select(".vacancy-card__title").first();
-                var linkElement = titleElement.child(0);
-                var vacancyName = titleElement.text();
-                var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                var dateElement = row.select(".vacancy-card__date").first();
-                var vacancyDateElement = dateElement.child(0);
-                var dateTime = String.format("%s", vacancyDateElement.attr("datetime"));
-
-                try {
-                    var description = retrieveDescription(link);
-                    System.out.printf("%s %s %s%n%s%n", vacancyName, link, dateTime, description);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+    public static void main(String[] args) {
+        HabrCareerParse habrCareerParse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> posts = habrCareerParse.list("https://career.habr.com");
+        for (Post post : posts) {
+            System.out.println(post);
         }
     }
 
